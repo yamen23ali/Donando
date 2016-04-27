af25736c71931f3681926ea6cc3c50d31d3134e5
Yamen Ali <yamen.ali@zalando-lounge.de>
2016-04-27 16:33:05 +0200

    Force json response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+  before_action :set_default_response_format
+
+  def set_default_response_format
+    request.format = :json
+  end
 end
--- app/controllers/demands_controller.rb
+++ app/controllers/demands_controller.rb
@@ -95,6 +95,7 @@ class DemandsController < ApplicationController
 
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_demand
       @demand = Demand.find(params[:id])
